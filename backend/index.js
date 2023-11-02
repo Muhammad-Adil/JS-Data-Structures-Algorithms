190f83c0150eaa5ddad97e7484ef574fba03b57c
Muhammad-Adil <adil.muhammad93@gmail.com>
2023-11-02 21:30:00 +0100

    Users complete

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Routes
+// SET TOKEN .
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "Unauthorize access" });
+  }
+  const token = authorization?.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "forbidden user or token has expired" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // MongoDB connection
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri = `mongodb://adil-yoga-0503:${process.env.DB_PASSWORD}@yoga-app.gpprp5e.mongodb.net`;
 //  // /?retryWrites=true&w=majority&appName=yoga-app`;
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-app.gpprp5e.mongodb.net/?retryWrites=true&w=majority&appName=yoga-app`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-app.gpprp5e.mongodb.net/?retryWrites=true&w=majority&appName=yoga-app`;
 // +srv
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -46,6 +65,96 @@ async function run() {
     //
     console.log("classes classesCollection", classesCollection);
     //
+
+    // Verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user.role === "admin") {
+        next();
+      } else {
+        return res
+          .status(401)
+          .send({ error: true, message: "Unauthorize access" });
+      }
+    };
+
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user.role === "instructor" || user.role === "admin") {
+        next();
+      } else {
+        return res
+          .status(401)
+          .send({ error: true, message: "Unauthorize access" });
+      }
+    };
+
+    app.post("/new-user", async (req, res) => {
+      const newUser = req.body;
+
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+    app.post("/api/set-token", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_SECRET, {
+        expiresIn: "24h",
+      });
+      res.send({ token });
+    });
+
+    // GET ALL USERS
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find({}).toArray();
+      res.send(users);
+    });
+    // GET USER BY ID
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+    // GET USER BY EMAIL
+    app.get("/user/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+    // Delete a user
+
+    app.delete("/delete-user/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    // UPDATE USER
+    app.put("/update-user/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+          role: updatedUser.option,
+          address: updatedUser.address,
+          phone: updatedUser.phone,
+          about: updatedUser.about,
+          photoUrl: updatedUser.photoUrl,
+          skills: updatedUser.skills ? updatedUser.skills : null,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
     // ! CLASSES ROUTES
 
@@ -147,7 +256,7 @@ async function run() {
 
     //
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
