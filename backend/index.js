497987895f0f1d8c7d9873e4e38c4b25d111b7d4
Muhammad-Adil <adil.muhammad93@gmail.com>
2023-11-02 11:30:00 +0100

    Classess route complete

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
-
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-app.gpprp5e.mongodb.net/?retryWrites=true&w=majority&appName=yoga-app`;
+// const uri = `mongodb://adil-yoga-0503:${process.env.DB_PASSWORD}@yoga-app.gpprp5e.mongodb.net`;
+//  // /?retryWrites=true&w=majority&appName=yoga-app`;
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-app.gpprp5e.mongodb.net/?retryWrites=true&w=majority&appName=yoga-app`;
+// +srv
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,10 +25,13 @@ const client = new MongoClient(uri, {
   },
 });
 
+//
+// console.log("client", client);
+//
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // Create DB and collections
     const database = client.db("yoga-app");
@@ -34,36 +41,109 @@ async function run() {
     const enrolledCollection = database.collection("enrolled");
     const paymentCollection = database.collection("payments");
     const appliedCollection = database.collection("applied");
-    client.connect();
+    await client.connect();
 
     //
+    console.log("classes classesCollection", classesCollection);
     //
 
     // ! CLASSES ROUTES
-    // verifyJWT,
-    // verifyInstructor,
-    app.post("/new-class", async (req, res) => {
+
+    app.post("/new-class", verifyJWT, verifyInstructor, async (req, res) => {
       const newClass = req.body;
       newClass.availableSeats = parseInt(newClass.availableSeats);
       const result = await classesCollection.insertOne(newClass);
       res.send(result);
     });
 
+    // GET ALL CLASSES ADDED BY INSTRUCTOR
+    app.get(
+      "/classes/:email",
+      verifyJWT,
+      verifyInstructor,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { instructorEmail: email };
+        const result = await classesCollection.find(query).toArray();
+        res.send(result);
+      }
+    );
+
     // GET ALL CLASSES
     app.get("/classes", async (req, res) => {
-      console.log("classes", classes);
+      const query = { status: "approved" };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/classes-manage", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Change status of a class
+    app.put("/change-status/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      console.log(req.body);
+      const reason = req.body.reason;
+      const filter = { _id: new ObjectId(id) };
+      console.log("🚀 ~ file: index.js:180 ~ app.put ~ reason:", reason);
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status,
+          reason: reason,
+        },
+      };
+      const result = await classesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    // * GET APPROVED CLASSES
+    app.get("/approved-classes", async (req, res) => {
       const query = { status: "approved" };
       const result = await classesCollection.find(query).toArray();
       res.send(result);
     });
 
-    // GET ALL CLASSES ADDED BY INSTRUCTOR
-    app.get("/classes/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { instructorEmail: email };
-      const result = await classesCollection.find(query).toArray();
+    // GET ALL INSTRUCTORS
+    app.get("/instructors", async (req, res) => {
+      const query = { role: "instructor" };
+      const result = await userCollection.find(query).toArray();
       res.send(result);
     });
+
+    // Update a class
+    app.put(
+      "/update-class/:id",
+      verifyJWT,
+      verifyInstructor,
+      async (req, res) => {
+        const id = req.params.id;
+        const updatedClass = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            name: updatedClass.name,
+            description: updatedClass.description,
+            price: updatedClass.price,
+            availableSeats: parseInt(updatedClass.availableSeats),
+            videoLink: updatedClass.videoLink,
+            status: "pending",
+          },
+        };
+        const result = await classesCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      }
+    );
 
     //
     // Send a ping to confirm a successful connection
@@ -72,8 +152,9 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
+    // console.log("finally", database);
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
